0d238a55fa21854a4679c72f74537a5f54d26814
gatesyp <ig11@zips.uakron.edu>
2016-04-03 19:48:51 -0700

    cleaned up build

--- build/CMakeFiles/printout.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/printout"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/printout.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
